65a48de6b8e13f58e4c6097e803287e8ecd29ba4
GoldenVaultXM <goldenvaultxm@gmail.com>
2026-05-28 10:12:29 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/Golden-vault-financial-dashboard/' // Must start and end with /
+  base: '/Golden-vault-financial-dashboard/'
 });
