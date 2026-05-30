42ed97be620be778ededffe3dba9f59e1119f334
GoldenVaultXM <goldenvaultxm@gmail.com>
2026-05-30 10:10:58 -0400

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,8 @@
+export const C = {
+  appName: "Golden Vault XM",
+};
+export const INSTRUMENT_DEFS = [
+  { pair: "BTC/USD", base: 65000, step: 0.001 },
+  { pair: "ETH/USD", base: 3500, step: 0.002 }
+];
+
